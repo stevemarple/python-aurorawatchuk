ad02758c18960cd3cec26fcea9e9f9b2386da6ff
Steve Marple <stevemarple@googlemail.com>
2017-01-23 23:03:01 +0000

    Update example to include the snapshot version of the class

--- aurorawatchuk/examples/get_status.py
+++ aurorawatchuk/examples/get_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import aurorawatchuk
+import aurorawatchuk.snapshot
 import datetime
 import logging
 import os
@@ -17,9 +18,11 @@ aurorawatchuk.user_agent = 'Python aurorawatchuk module (%s)' % os.path.basename
 # Creating an AuroraWatchUK object. Its fields are accessors which return the latest status and other information.
 aw = aurorawatchuk.AuroraWatchUK()
 
+
+
 # Print the current status level, and when it was updated.
-print('Current status level: ' + aw.status)
-print('Current status updated: ' + aw.status_updated.strftime('%Y-%m-%d %H:%M:%S'))
+print('Current status level: ' + aw.status.level)
+print('Current status updated: ' + aw.status.updated.strftime('%Y-%m-%d %H:%M:%S'))
 
 # Print the color, meaning and description for each status level
 print('Status descriptions:')
@@ -32,8 +35,18 @@ for status_level in desc:
 
 
 print('----------------------------')
+# Take a snapshot of the AuroraWatch UK status. The information will not be updated
+aw_ss = aurorawatchuk.snapshot.AuroraWatchUK()
 
 while True:
     now = datetime.datetime.utcnow()
-    print('{now:%Y-%m-%d %H:%M:%S}: {status}'.format(now=now, status=aw.status))
+    # Print the current status
+    print('{now:%Y-%m-%d %H:%M:%S}'.format(now=now))
+    print('Current status:   level={status.level} updated={status.updated:%Y-%m-%d %H:%M:%S}'
+          .format(status=aw.status))
+
+    # Print the snapshot taken earlier
+    print('Earlier snapshot: level={status.level} updated={status.updated:%Y-%m-%d %H:%M:%S}'
+          .format(status=aw_ss.status))
+
     time.sleep(10)
