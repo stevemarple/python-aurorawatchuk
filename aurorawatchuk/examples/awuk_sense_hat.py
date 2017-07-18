45a1d38d57ec6a40ee2b6cb4b9e53064c51a8a62
Steve Marple <stevemarple@googlemail.com>
2017-07-18 18:02:03 +0100

    Remove struct import

--- aurorawatchuk/examples/awuk_sense_hat.py
+++ aurorawatchuk/examples/awuk_sense_hat.py
@@ -4,7 +4,6 @@
 import aurorawatchuk
 from sense_hat import SenseHat
 import logging
-import struct
 import time
 
 logger = logging.getLogger(__name__)
@@ -14,7 +13,7 @@ awuk = aurorawatchuk.AuroraWatchUK()
 # Convert RGB color strings (#RRGGBB) to tuples
 sense_color = {}
 for c, v in awuk.descriptions.items():
-    sense_color[c] = tuple(int(v['color'][x:x+2], 16) for x in range(1,7,2))
+    sense_color[c] = tuple(int(v['color'][x:x+2], 16) for x in range(1, 7, 2))
 
 while True:
     sense.set_pixels([sense_color[awuk.status.level]]*64)
